588cfd8a8f0d72135f19f2da539d656fa67c573b
kotsimp <kotsimp@gmail.com>
2026-06-26 12:02:56 +0300

    chore: update conf-sample.php to include db_connect and CAS admin parameters

--- conf-sample.php
+++ conf-sample.php
@@ -8,14 +8,39 @@ $pruid = '';
 $prem1 = '';
 $prem2 = '';
 
+// Admin Usernames (CAS uid)
+$prAdmin1 = '';
+$prAdmin2 = '';
+
 // DB credentials
 $prDbhost = 'localhost';
-$prDbname = 'progs';
-$prDbusername = '';
+$prDbname = 'school-programs';
+$prDbusername = 'root';
 $prDbpassword = '';
 
 $prTable = 'progs';
 $schTable = 'progs_schools';
 
-
+function db_connect() {
+    global $prDbhost, $prDbusername, $prDbpassword, $prDbname;
+    // Temporarily turn off exception reporting for connection fallback
+    $previous_reporting = mysqli_report(MYSQLI_REPORT_OFF);
+    
+    $conn = @new mysqli($prDbhost, $prDbusername, $prDbpassword, $prDbname);
+    if ($conn && !$conn->connect_error) {
+        $conn->set_charset("utf8");
+        mysqli_report($previous_reporting);
+        return $conn;
+    }
+    
+    $conn = @new mysqli($prDbhost, $prDbusername, '', $prDbname);
+    if ($conn && !$conn->connect_error) {
+        $conn->set_charset("utf8");
+        mysqli_report($previous_reporting);
+        return $conn;
+    }
+    
+    mysqli_report($previous_reporting);
+    die("Database connection failed: Access denied or database does not exist.");
+}
 ?>
